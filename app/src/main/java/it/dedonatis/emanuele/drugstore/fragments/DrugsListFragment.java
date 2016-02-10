6965694048eb0f7636952a5173922aa79b0b176b
Emanuele De Donatis <emanuele.dedonatis@gmail.com>
2016-02-10 13:57:25 +0100

    Share drug

--- app/src/main/java/it/dedonatis/emanuele/drugstore/fragments/DrugsListFragment.java
+++ app/src/main/java/it/dedonatis/emanuele/drugstore/fragments/DrugsListFragment.java
@@ -32,6 +32,7 @@ import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.OnItemClickListener;
+import com.unnamed.b.atv.model.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +44,8 @@ import it.dedonatis.emanuele.drugstore.adapters.DialogBottomAdapter;
 import it.dedonatis.emanuele.drugstore.adapters.DrugRecyclerAdapter;
 import it.dedonatis.emanuele.drugstore.data.DataContract;
 import it.dedonatis.emanuele.drugstore.models.Drug;
+import it.dedonatis.emanuele.drugstore.models.DrugPackage;
+import it.dedonatis.emanuele.drugstore.models.DrugSubpackage;
 import it.dedonatis.emanuele.drugstore.utils.Dialogs;
 
 public class DrugsListFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>, DrugRecyclerAdapter.DrugClickListener, SearchView.OnQueryTextListener {
@@ -60,6 +63,23 @@ public class DrugsListFragment extends Fragment implements LoaderManager.LoaderC
     public static final int COL_DRUG_ID = 0;
     public static final int COL_DRUG_NAME = 1;
     public static final int COL_DRUG_API = 2;
+
+    private static final String[] PACKAGE_COLUMNS = {
+            DataContract.PackageEntry.TABLE_NAME + "." + DataContract.PackageEntry._ID,
+            DataContract.PackageEntry.COLUMN_DESCRIPTION
+    };
+    public static final int COL_PACKAGE_ID = 0;
+    public static final int COL_PACKAGE_DESCRIPTION = 1;
+
+
+    private static final String[] SUBPACKAGE_COLUMNS = {
+            DataContract.SubpackageEntry.TABLE_NAME + "." + DataContract.SubpackageEntry._ID,
+            DataContract.SubpackageEntry.COLUMN_DOSES_LEFT
+    };
+
+    public static final int COL_SUBPACKAGE_DOSES_LEFT = 1;
+
+
 
     private List<Drug> mDrugs = new ArrayList<>();
     private DrugRecyclerAdapter mAdapter;
@@ -208,7 +228,7 @@ public class DrugsListFragment extends Fragment implements LoaderManager.LoaderC
                 Log.d(LOG_TAG, "Click on item " + position);
                 switch (position) {
                     case DialogBottomAdapter.SHARE_POSITION:
-                        Log.d(LOG_TAG, "share drug " + drugId);
+                        shareDrug(drugId, name, api);
                         dialog.dismiss();
                         break;
                     case DialogBottomAdapter.EDIT_POSITION:
@@ -268,13 +288,51 @@ public class DrugsListFragment extends Fragment implements LoaderManager.LoaderC
                             }
                 ).build();
 
-                    View view = dialog.getView();
-                    EditText drugName = (EditText) view.findViewById(R.id.dialog_edit_drug_name);
-                    EditText drugApi = (EditText) view.findViewById(R.id.dialog_edit_drug_api);
-                    drugName.setText(name);
-                    drugApi.setText(api);
-                    dialog.show();
+        View view = dialog.getView();
+        EditText drugName = (EditText) view.findViewById(R.id.dialog_edit_drug_name);
+        EditText drugApi = (EditText) view.findViewById(R.id.dialog_edit_drug_api);
+        drugName.setText(name);
+        drugApi.setText(api);
+        dialog.show();
 
-                }
     }
+
+    private void shareDrug(final long drugId, final String name, final String api) {
+        String textToSend = name + " (" + api + ") ";
+
+        Cursor packageCursor = getActivity().getContentResolver().query(
+                DataContract.PackageEntry.buildPackagesFromDrug(drugId),
+                        PACKAGE_COLUMNS,
+                null,
+                null,
+                DataContract.PackageEntry.COLUMN_DESCRIPTION + " ASC");
+
+        while (packageCursor.moveToNext()) {
+            long packageId = packageCursor.getLong(COL_PACKAGE_ID);
+
+            Cursor subpackageCursor = getActivity().getContentResolver().query(
+                    DataContract.SubpackageEntry.buildSubackagesFromPackage(packageId),
+                    SUBPACKAGE_COLUMNS,
+                    null,
+                    null,
+                    null
+            );
+
+            int dosesLeft = 0;
+
+            while(subpackageCursor.moveToNext()) {
+              dosesLeft += subpackageCursor.getInt(COL_SUBPACKAGE_DOSES_LEFT);
+            }
+
+
+                String line = "- " + dosesLeft + " x " + packageCursor.getString(COL_PACKAGE_DESCRIPTION);
+                textToSend += "\n" + line;
+        }
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textToSend);
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+}
 
